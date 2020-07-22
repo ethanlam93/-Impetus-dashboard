84a6ade8cd80f733284267207d906b7fc923f337
TJFitz <tjfitz@comcast.net>
2020-07-21 20:23:13 -0400

    learning pull requests

---
+++ bookapi.js
@@ -0,0 +1,13 @@
+// API Key for Google Books AIzaSyCGDQmqkLhwEgc5Zav3Uezlzh1qXFBdbuA
+
+var searchTerm = "harry potter";
+$.ajax({
+  url:
+    "https://www.googleapis.com/books/v1/volumes?q=" +
+    searchTerm +
+    "&download=epub&key=AIzaSyCGDQmqkLhwEgc5Zav3Uezlzh1qXFBdbuA",
+  method: "GET",
+}).then(function (response) {
+  console.log(response);
+  $("body").append($("<div>").text(response.items[1].volumeInfo.title));
+});
