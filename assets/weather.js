24c859579f65041956fe5790d798d29bfe20bf3b
Jacob Ashworth <jeashwor@gmail.com>
2020-07-23 21:16:24 -0400

    added ids to html to target with javascript file

--- assets/weather.js
+++ assets/weather.js
@@ -1,7 +1,18 @@
+var headerNameEl = $("#headerName");
+var sundayEl = $("#0");
+var mondayEl = $("#1");
+var tuesdayEl = $("#2");
+var wednesdayEl = $("#3");
+var thursdayEl = $("#4");
+var fridayEl = $("#5");
+var saturdayEl = $("#6");
+
 var dateLocation = moment().format("d");
 console.log(dateLocation);
 
 var yourZip = 30047;
+var lat = 0;
+var lon = 0;
 
 function getWeather () {
     var queryUrl = "https://api.openweathermap.org/data/2.5/forecast?zip=" + yourZip + "&appid=b2cb9091c77c412d1dede93b0ba6839c";
@@ -16,4 +27,7 @@ $.ajax({
 })
 };
 
+function getWeatherHistory () {
+    v
+}
 getWeather();
