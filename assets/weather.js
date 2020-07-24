ec3744a61e7314ae84acba4418f1eb9697082ec0
Jacob Ashworth <jeashwor@gmail.com>
2020-07-24 16:53:41 -0400

    added elements to retreive field inputs

--- assets/weather.js
+++ assets/weather.js
@@ -1,4 +1,6 @@
 var headerNameEl = $("#headerName");
+var userNameEl = $("#userName");
+var zipCodeEl = $("#zipCode");
 var sundayEl = $("#0");
 var mondayEl = $("#1");
 var tuesdayEl = $("#2");
@@ -9,12 +11,29 @@ var saturdayEl = $("#6");
 
 var dateLocation = moment().format("d");
 console.log(dateLocation);
+var d = new Date();
+d.setDate(d.getDate()-7);
+console.log(d);
 
 var yourZip = 30047;
 var lat = 0;
 var lon = 0;
 var dt = 0;
 
+// Check to see if values previous entry of username and zipcode.  
+var dashboardUserName = JSON.parse(localStorage.getItem("dun"));
+var dashboardUserZip = JSON.parse(localStorage.getItem("duz"));
+
+// Validate if storage exist, if proceed to dashboard. 
+if (dashboardUserName !== null && dashboardUserZip !== null) {
+    previousCitiesSearched = storedCities;
+    $("#currentCityWeather").empty();
+    cityToSearch = previousCitiesSearched[0];
+    createCityButtons();
+    mainWeatherHeader();
+} else {
+    previousCitiesSearched = [];
+};
 
 function getWeather() {
     var queryUrl = "https://api.openweathermap.org/data/2.5/forecast?zip=" + yourZip + "&appid=b2cb9091c77c412d1dede93b0ba6839c";
@@ -36,11 +55,39 @@ function getFiveDayForecast() {
         method: "Get"
     }).then (function (response) {
         console.log(response);
+        dt = response.current.dt;
+        getWeatherHistory();
     })
-}
+};
 
 function getWeatherHistory() {
-    var historyQuery = "https://api.openweather.org/data/2.5/oncall/timeout?"
-}
+    var historyQuery = "https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=" + lat + "&lon=" + lon + "&dt=" + dt + "&units=imperial&appid=b2cb9091c77c412d1dede93b0ba6839c";
+    $.ajax({
+        url: historyQuery,
+        method: "Get"
+    }).then (function (response) {
+        console.log(response);
+    })
+};
 
-getWeather();
+function setUserName() {
+    user
+};
+
+// Click Event to submit form data.
+$(".#submitBtn").on("click", function (event) {
+    event.preventDefault();
+    // $("").removeClass("d-none");
+    setUserName();
+    getWeather();
+});
+
+// keypress event added to enter key within text input
+$(document).keypress(function (event) {
+    if (event.keyCode == 13) {
+        event.preventDefault();
+        // $("").removeClass("d-none");
+        setUserName();
+        getWeather();
+    }
+});
