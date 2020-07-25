1f9161a2d79adfdda86298588b8c1c03668025c1
TJFitz <tjfitz@comcast.net>
2020-07-25 09:40:32 -0400

    save before pull

--- assets/bookapi.js
+++ assets/bookapi.js
@@ -1,23 +1,37 @@
-// API Key for Google Books AIzaSyCGDQmqkLhwEgc5Zav3Uezlzh1qXFBdbuA
+var searchTerm = localStorage.getItem("bookSearch");
+if (searchTerm === null) {
+  searchTerm = "positivity";
+}
+// Click handler for book search submit button
 
-var searchTerm = "self help";
-$.ajax({
-  url:
-    "https://www.googleapis.com/books/v1/volumes?q=subject:" +
-    searchTerm +
-    "&download=epub&key=AIzaSyCGDQmqkLhwEgc5Zav3Uezlzh1qXFBdbuA",
-  method: "GET",
-}).then(function (response) {
-  randNum = Math.floor(Math.random() * 10);
-  console.log(response);
-  console.log(response.items[randNum].volumeInfo.title);
-  $("#bookTitle").text(response.items[randNum].volumeInfo.title);
-  $("#bookCover").attr(
-    "src",
-    response.items[randNum].volumeInfo.imageLinks.thumbnail
-  );
-  $("#bookLink").attr(
-    "href",
-    response.items[randNum].volumeInfo.canonicalVolumeLink
-  );
+$(document).on("click", "#bookSearchSubmit", function () {
+  var inputField = $("#bookSearchbar").val();
+  searchTerm = inputField;
+  localStorage.setItem("bookSearch", searchTerm);
+  getBook();
 });
+
+// Function for making ajax call and displaying book info
+
+function getBook() {
+  $.ajax({
+    url:
+      "https://www.googleapis.com/books/v1/volumes?q=" +
+      searchTerm +
+      "&langRestrict=en&download=epub&key=AIzaSyCGDQmqkLhwEgc5Zav3Uezlzh1qXFBdbuA",
+    method: "GET",
+  }).then(function (response) {
+    randNum = Math.floor(Math.random() * 10);
+    $("#bookTitle").text(response.items[randNum].volumeInfo.title);
+    $("#bookCover").attr(
+      "src",
+      response.items[randNum].volumeInfo.imageLinks.thumbnail
+    );
+    $("#bookLink").attr(
+      "href",
+      response.items[randNum].volumeInfo.canonicalVolumeLink
+    );
+  });
+}
+
+getBook();
