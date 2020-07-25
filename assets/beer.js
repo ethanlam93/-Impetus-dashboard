ebc680190120c3f701525abe30900c271f147b59
TJFitz <tjfitz@comcast.net>
2020-07-25 12:52:31 -0400

    save before merge request

--- assets/beer.js
+++ assets/beer.js
@@ -1,29 +1,16 @@
-$(document).ready(function(){
-    var beer="https://api.punkapi.com/v2/beers/random";
-        $.ajax({
-            url: beer,
-            method: "GET"
-    
-          }).then(function(data){
-           console.log(data);
-           console.log(data[0].name);
-           console.log(data[0].tagline);
-           console.log(data[0].description);
-           console.log(data[0].image_url);
-        $("#name").text("Beer Name: "+data[0].name);
-        $("#description").text("Description: "+data[0].description);
-        $("#image").attr("src", data[0].image_url);
-    
-          });
-        
-        
-        
-        
-        
-    });
-    
-    
-    
-    
-    
-    
+$(document).ready(function () {
+  var beer = "https://api.punkapi.com/v2/beers/random";
+  $.ajax({
+    url: beer,
+    method: "GET",
+  }).then(function (data) {
+    console.log(data);
+    console.log(data[0].name);
+    console.log(data[0].tagline);
+    console.log(data[0].description);
+    console.log(data[0].image_url);
+    $("#name").text("Beer Name: " + data[0].name);
+    $("#description").text("Description: " + data[0].description);
+    $("#image").attr("src", data[0].image_url);
+  });
+});
