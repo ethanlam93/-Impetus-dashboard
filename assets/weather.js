f5dd417ac3d6cc09cb2ad05352cc77b0a35d682d
TJFitz <tjfitz@comcast.net>
2020-07-25 19:34:51 -0400

    reorganized and added styling

--- assets/weather.js
+++ assets/weather.js
@@ -1,21 +1,6 @@
 var headerNameEl = $("#headerName");
 var userNameEl = $("#userName");
 var zipCodeEl = $("#zipCode");
-
-// gets current date and day and then creates elements in html if
-var currentDate = moment().format("MM-DD-YY");
-var currentDOW = moment().format("dddd");
-for (var j = 0; j < 7; j++) {
-  $("#" + j).empty();
-  var displayDOW = moment().add(j, "day").format("dddd");
-  var displayDate = moment().add(j, "day").format("MM-DD-YY");
-  var dayEl = $("<h3>");
-  dayEl.addClass("day").text(displayDOW);
-  var dateEl = $("<h4>");
-  dateEl.addClass("date").text(displayDate);
-  $("#" + j).append(dayEl, dateEl);
-}
-
 var userName = "";
 var userZip = 0;
 var lat = 0;
