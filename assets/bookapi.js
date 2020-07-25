72aa200ac3c3bb1189b9d37915dc0fb08b9393f6
TJFitz <tjfitz@comcast.net>
2020-07-24 20:34:23 -0400

    working on book display

--- assets/bookapi.js
+++ assets/bookapi.js
@@ -1,13 +1,23 @@
 // API Key for Google Books AIzaSyCGDQmqkLhwEgc5Zav3Uezlzh1qXFBdbuA
 
-// var searchTerm = "harry potter";
-// $.ajax({
-//   url:
-//     "https://www.googleapis.com/books/v1/volumes?q=" +
-//     searchTerm +
-//     "&download=epub&key=AIzaSyCGDQmqkLhwEgc5Zav3Uezlzh1qXFBdbuA",
-//   method: "GET",
-// }).then(function (response) {
-//   console.log(response);
-//   $("body").append($("<div>").text(response.items[1].volumeInfo.title));
-// });
+var searchTerm = "self help";
+$.ajax({
+  url:
+    "https://www.googleapis.com/books/v1/volumes?q=subject:" +
+    searchTerm +
+    "&download=epub&key=AIzaSyCGDQmqkLhwEgc5Zav3Uezlzh1qXFBdbuA",
+  method: "GET",
+}).then(function (response) {
+  randNum = Math.floor(Math.random() * 10);
+  console.log(response);
+  console.log(response.items[randNum].volumeInfo.title);
+  $("#bookTitle").text(response.items[randNum].volumeInfo.title);
+  $("#bookCover").attr(
+    "src",
+    response.items[randNum].volumeInfo.imageLinks.thumbnail
+  );
+  $("#bookLink").attr(
+    "href",
+    response.items[randNum].volumeInfo.canonicalVolumeLink
+  );
+});
