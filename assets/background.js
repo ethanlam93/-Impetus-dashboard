3709651237b883f4c3fcfada9608394d6c9f2c25
TJFitz <tjfitz@comcast.net>
2020-07-28 11:51:55 -0400

    save before checkout

---
+++ assets/background.js
@@ -0,0 +1,54 @@
+var currentBackground;
+var imageArray = [
+  [
+    "./assets/images/day_clear.jpg",
+    "./assets/images/day_rain.jpg",
+    "./assets/images/day_snow.jpg",
+    "./assets/images/day_storms.jpg",
+  ],
+  [
+    "./assets/images/night_clear.jpg",
+    "./assets/images/night_storms.jpg",
+    "./assets/images/night_snow.jpg",
+  ],
+];
+function localWeather() {
+  var queryUrl =
+    "https://api.openweathermap.org/data/2.5/weather?zip=" +
+    userZip +
+    "&appid=b2cb9091c77c412d1dede93b0ba6839c";
+  $.ajax({
+    url: queryUrl,
+    method: "Get",
+  }).then(function (response) {
+    console.log(response);
+    var currentWeather = response.weather[0].main;
+    var currentTime = moment().format("X");
+    var currentSunrise = response.sys.sunrise;
+    var currentSunset = response.sys.sunset;
+    if (currentTime > currentSunrise && currentTime < currentSunset) {
+      //daytime logic
+      if (currentWeather === "Storms") {
+        currentBackground = imageArray[0][3];
+      } else if (currentWeather === "Rain") {
+        currentBackground = imageArray[0][1];
+      } else if (currentWeather === "Snow") {
+        currentBackground = imageArray[0][2];
+      } else {
+        currentBackground = imageArray[0][0];
+      }
+    } else {
+      //nighttime logic
+      if (currentWeather === "Storms") {
+        currentBackground = imageArray[1][1];
+      } else if (currentWeather === "Snow") {
+        currentBackground = imageArray[1][2];
+      } else {
+        currentBackground = imageArray[1][0];
+      }
+    }
+    console.log(currentBackground);
+    $("body").css("background-image", "url(" + currentBackground + ")");
+  });
+}
+localWeather();
